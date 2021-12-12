29c4fb6d45c10921c0defb5c61640878228d2a2e
Aleksandr Zheltishev <azheltishevs@gmail.com>
2021-12-12 21:29:10 +0200

    MapstructConfig.java

---
+++ src/main/java/com/mapstructdemo/mapstruct/MapstructConfig.java
@@ -0,0 +1,19 @@
+package com.mapstructdemo.mapstruct;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(
+        // source has field X, target doesn't have X
+        unmappedSourcePolicy = ReportingPolicy.IGNORE, // default is IGNORE
+
+        // target has field Y, source doesn't have Y
+        unmappedTargetPolicy = ReportingPolicy.WARN, // default is WARN
+
+        // field Z in target is not of the same type as in source
+        typeConversionPolicy = ReportingPolicy.IGNORE, // default is IGNORE
+
+        componentModel = "spring" // default is "default" ¯\_(ツ)_/¯
+)
+public class MapstructConfig {
+}
